10466a70c0e6523edfafee76658c301ebe335847
olikelly00 <oliverkelly1995@hotmail.co.uk>
2024-08-13 11:50:01 +0100

    project setup and first react component

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,62 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Header() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <header>
+            <h1>🔥 Fireplace Palace</h1>
+        </header>
+    </>
+  )
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      <body>
+        {Header()}
+        <div className="wrapper">
+        <main>
+            <section className="hero-1">
+                <img src="src/assets/hero-desktop.png" alt="fireplace"></img>
+                <div className="call-to-action">
+                    <h2>Discover the Perfect Fireplace...</h2>
+                    <p>Book consultation: <span className="contact-number">0121 345 6789</span></p>
+                </div>
+                
+            </section>
+            <section className="how-it-works">
+                <h2>How it works.</h2>
+                
+                <img src="src/assets/how-it-works-1.png" alt="article-1"></img>
+                <h5>Give us a call...</h5>
+                <p>Call us and book in a "Design Consultation" on a date and time to suit.</p>
+
+                <img src="src/assets/how-it-works-2.png" alt="article-2"></img>
+                <h5>We come to you...</h5>
+                <p>We come to your home to do an assessment of the space and to your style preference.</p>
+
+                <img src="src/assets/how-it-works-3.png" alt="article-3"></img>
+                <h5>We recommend...</h5>
+                <p>We send you a bespoke set of fireplace recommendation.</p>
+
+            </section>
+        </main>
+    </div>
+        <footer className="footer">
+            <p>Find us on</p>
+                <p>Facebook </p>
+                <p>Instagram </p>
+                <p>TikTok </p>
+             
+              <p> (C) Fireplace Palace 2024</p>
+              <p>info@firepalace.co.uk</p>
+
+        </footer>
+    
+</body>
     </>
   )
 }
