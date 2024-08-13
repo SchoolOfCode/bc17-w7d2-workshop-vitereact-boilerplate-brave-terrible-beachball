f119f46a2d212c556dc5f32a14c307f43e6e7dc3
Jordan <jordanfhoughton@gmail.com>
2024-08-13 18:22:15 +0100

    jsx files created

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+      <>
+      <header>
+              <h1>🔥 Fireplace Palace</h1>
+          </header>
+      </>
+    )
+  }
---
+++ src/components/HeroOne/HeroImage/heroOneImg.jsx
@@ -0,0 +1,8 @@
+export function HeroOneImage() {
+    const [image] = useState({src:"src/assets/hero-desktop.png", alt:"fireplace"})
+    return (
+      <>
+      <img src={image.src} alt={image.alt}></img>
+      </> )
+    
+  }
