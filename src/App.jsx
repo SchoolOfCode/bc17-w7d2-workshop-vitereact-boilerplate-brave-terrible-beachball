9a3fadb74b5d7a2434bd2e7324eb9ca67b2d5e35
Jordan <jordanfhoughton@gmail.com>
2024-08-13 18:17:56 +0100

    created component folders and added compenent jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -2,54 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Header} from './components/Header/header.jsx'
+import {HeroOneImage} from './components/HeroOne/HeroImage/heroOneImg.jsx'
+import {HeroOneText} from './components/HeroOne/HeroText/HeroOneText.jsx'
+import {HowItWorks} from './components/HowItWorks/howItWorks.jsx'
 
-function Header() {
-  return (
-    <>
-    <header>
-            <h1>🔥 Fireplace Palace</h1>
-        </header>
-    </>
-  )
-}
+
 
 function HeroOne() {
   return(
   <>
   <section className="hero-1">
-                <img src="src/assets/hero-desktop.png" alt="fireplace"></img>
-                <div className="call-to-action">
-                    <h2>Discover the Perfect Fireplace...</h2>
-                    <p>Book consultation: <span className="contact-number">0121 345 6789</span></p>
-                </div>
+                  <HeroOneImage />
+                  <HeroOneText/>
                 
             </section>
             </>
   )
 }
 
-function HowItWorks() {
-  const [articleList] = useState([
-    {image: "src/assets/how-it-works-1.png", heading: "Give us a call...", bodyText: "Call us and book in a \'Design Consultation\' on a date and time to suit.", id: 1},
-    {image: "src/assets/how-it-works-1.png", heading: "We come to you...", bodyText: "We come to your home to do an assessment of the space and to your style preference.", id: 2},
-    {image: "src/assets/how-it-works-1.png", heading: "We recommend...", bodyText: "We send you a bespoke set of fireplace recommendation.", id: 3}
-  ]);
-  return ( 
-    <>
-<section className="how-it-works">
-  { <h2>How it works.</h2> }
-  {articleList.map((article) => (
-    <div className="article-preview" key={article.id}>
-      <img src={article.image}></img>
-      <h2>{ article.heading }</h2>
-      <p>{ article.bodyText }</p>
-    </div>
 
-  ))}
-  </section>
-  </>
-  );
-}
                 
 
 function Footer() {
